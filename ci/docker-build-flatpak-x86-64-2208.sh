2e16887e2fc01be2ffbc12b502b1d85bb6012f2d
dsr <bdbcat@yahoo.com>
2023-04-15 18:38:44 -0400

    TLS docker fedora test #15

--- ci/docker-build-flatpak-x86-64-2208.sh
+++ ci/docker-build-flatpak-x86-64-2208.sh
@@ -24,6 +24,8 @@ flatpak remote-add --user  flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install --user -y  flathub org.freedesktop.Sdk//22.08  >/dev/null
 flatpak install --user -y  flathub org.freedesktop.Platform/x86_64/22.08  >/dev/null
 flatpak install --user -y  https://flathub.org/repo/appstream/org.opencpn.OpenCPN.flatpakref >/dev/null
+flatpak list
+
 rm -rf build && mkdir build && cd build
 git config --global --add safe.directory /root/project
 cmake -DOCPN_FLATPAK=ON ..
