c00ad499e64fd833b6b38159ef71137d83e2fd6a
dsr <bdbcat@yahoo.com>
2023-07-05 22:44:59 -0400

    Version 1.20

--- src/s63_pi.h
+++ src/s63_pi.h
@@ -42,7 +42,7 @@
 #include "TexFont.h"
 
 #define     PLUGIN_VERSION_MAJOR    1
-#define     PLUGIN_VERSION_MINOR    19
+#define     PLUGIN_VERSION_MINOR    20
 
 #define     MY_API_VERSION_MAJOR    1
 #define     MY_API_VERSION_MINOR    16
